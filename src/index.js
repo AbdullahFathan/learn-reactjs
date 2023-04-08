24fb3facc5e13260c105117ca3e7538fa2d719b6
AbdullahFathan <abdullahfathan013@gmail.com>
2023-04-08 21:04:34 +0700

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./index.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function BookList() {
+  return (
+    <section className="bookList">
+      <Book jobs="developer" />
+      <Book title="random title" number={22} />
+    </section>
+  );
+}
+
+const Book = (props) => {
+  return (
+    <article className="book">
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/81fyoFoaxlL._AC_UL600_SR600,400_.jpg"
+        alt="Dog Man: Twenty Thousand Fleas Under the Sea"
+      />
+      <h2>Dog Man: Twenty Thousand Fleas Under the Sea</h2>;
+      <h4
+        style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.5rem" }}
+      >
+        {" "}
+        Dav Pilkey{" "}
+      </h4>
+      <p>{props.jobs}</p>
+      <p>{props.title}</p>
+      <p>{props.number}</p>
+    </article>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<BookList />);
